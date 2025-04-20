568c9bc99aafa9041836fcd1fbbad6e2c8074919
hrowlxb <hrowlxb516@gmail.com>
2025-04-20 20:23:45 +0900

    passy project

---
+++ passy-backend/src/main/java/com/hrowlxb/passy_backend/PassyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hrowlxb.passy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassyBackendApplication.class, args);
+	}
+
+}
---
+++ passy-backend/src/test/java/com/hrowlxb/passy_backend/PassyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrowlxb.passy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
