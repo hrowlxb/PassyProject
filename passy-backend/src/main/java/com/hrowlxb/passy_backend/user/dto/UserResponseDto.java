5385e375e96d3388046721a3013a7f19ceeaedf1
hrowlxb <hrowlxb516@gmail.com>
2025-04-22 12:01:57 +0900

    add file

---
+++ passy-backend/src/main/java/com/hrowlxb/passy_backend/user/dto/UserResponseDto.java
@@ -0,0 +1,10 @@
+package com.hrowlxb.passy_backend.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserResponseDto {
+    private String token;
+}
